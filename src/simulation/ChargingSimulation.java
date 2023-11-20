aff27bd51a80cc51c7bcd844d24faa82f62ffb48
Varsha Vijayan <mevarsha.v94@gmail.com>
2023-11-20 15:42:32 +0530

    managing the log files - create, move, delete, archive

--- src/simulation/ChargingSimulation.java
+++ src/simulation/ChargingSimulation.java
@@ -18,5 +18,38 @@ public class ChargingSimulation {
         logger.log("Hello World!");
 
 	}
+	 private static void logManagement(String task) {
+	        if (task.equals("write")) {
+	            // 1. logging system functionality
+	            LogManager.logSystemFunctionality("System initialization.");
+	            LogManager.logSystemFunctionality("Write log entries to a file.");
+	            LogManager.logSystemFunctionality("Move log files to an archive folder.");
+	            LogManager.logSystemFunctionality("Delete log files.");
+	            LogManager.logSystemFunctionality("adding a new charging station");
+	            LogManager.logSystemFunctionality("System shutdown.");
+
+
+	            // 2. logging charging station functionality
+	            LogManager.logChargingStationFunctionality("Charging session started for Car-001.");
+	            LogManager.logChargingStationFunctionality("Charging session ended for Car-001.");
+	            LogManager.logChargingStationFunctionality("Weather checked.");
+	            LogManager.logChargingStationFunctionality("charging station is full");
+	            LogManager.logChargingStationFunctionality("a new location had been freed");
+
+
+	            // 3. logging energy management functionality
+	            LogManager.logEnergyManagementFunctionality("Wind energy utilized for charging.");
+	            LogManager.logEnergyManagementFunctionality("Solar energy utilized for charging.");
+	            LogManager.logEnergyManagementFunctionality("Fossil fuel energy utilized for charging.");
+	        }
+
+	        if (task.equals("archive")) {
+	            // Move log files to an archive folder
+	            LogManager.moveSystemLogToArchive();
+	            LogManager.moveChargingStationLogToArchive();
+	            LogManager.moveEnergyManagementLogToArchive();
+	        }
+
+	    }
 
 }
